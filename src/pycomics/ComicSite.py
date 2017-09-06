70281c413ba7b5b1cc91bc3935472edeba2d3192
oneshan <oneshan@users.noreply.github.com>
2017-09-06 11:22:51 -0700

    Modify code style and python3 Compatibility

--- src/pycomics/ComicSite.py
+++ src/pycomics/ComicSite.py
@@ -1,149 +1,173 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-import urllib2
-import sys
-import re
 import os
+import re
+import sys
+import requests
+
+isPy2 = sys.version_info < (3, 0)
+
 
 class ComicSite(object):
-	opener = urllib2.build_opener()
-	opener.addheaders = [
-		("User-Agent", "Mozilla/5.0 Gecko/2010 Firefox/5"),
-		("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"),
-		("Accept-Language", "en-us,en;q=0.5"),
-		("Accept-Encoding", "deflate"),
-	]
-	root = os.path.expanduser("~")
-	os.chdir(root)
+    encoding = "utf-8"
+    header = {
+        "User-Agent": "Mozilla/5.0 Gecko/2010 Firefox/5",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+        "Accept-Language": "en-us,en;q=0.5",
+        "Accept-Encoding": "deflate",
+    }
+    root = os.path.expanduser("~")
+    os.chdir(root)
 
-	def openTrackList(self):
-		p = "%s-tracklist.txt"%self.path()
-		if not os.path.exists(p):
-			open(p, 'a').close()
-		if sys.platform == 'darwin':
-			os.system('open %s'%p)
-		elif sys.platform == 'win32':
-			os.system(p)
-		
-	def trackList(self):
-		p = "%s-tracklist.txt"%self.path()
-		if os.path.exists(p):
-			return map(str.strip, open(p).readlines())
-		else:
-			return []
+    def openTrackList(self):
+        p = "%s-tracklist.txt" % self.path()
+        if not os.path.exists(p):
+            open(p, 'a').close()
+        if sys.platform == 'darwin':
+            os.system('open %s' % p)
+        elif sys.platform == 'win32':
+            os.system(p)
 
-	def execJs(self, s):
-		import tempfile, commands
-		fd, path = tempfile.mkstemp(suffix='.js')
-		f = os.fdopen(fd, 'w')
-		if sys.platform == 'win32':
-			s = s.replace('print', 'WScript.Echo')
-		f.write(s)
-		f.close()
-		if sys.platform == 'darwin':
-			ret = commands.getoutput("/System/Library/Frameworks/JavaScriptCore.framework/Versions/Current/Resources/jsc %s"%path)
-		elif sys.platform == 'win32':
-			ret = ''.join(os.popen("cscript %s"%path).readlines()[3:]).strip()
-		os.remove(path)
-		return ret
+    def trackList(self):
+        p = "%s-tracklist.txt" % self.path()
+        if os.path.exists(p):
+            return map(str.strip, open(p).readlines())
+        else:
+            return []
 
-	def path(self):
-		return self.__class__.__name__
+    def execJs(self, s):
+        import subprocess
+        import tempfile
+        fd, path = tempfile.mkstemp(suffix='.js')
+        f = os.fdopen(fd, 'w')
+        if sys.platform == 'win32':
+            s = s.replace('print', 'WScript.Echo')
+        f.write(s)
+        f.close()
+        if sys.platform == 'darwin':
+            jsc_cmd = ("/System/Library/Frameworks/JavaScriptCore.framework/"
+                       "Versions/Current/Resources/jsc " + path)
+            ret = subprocess.check_output(jsc_cmd, shell=True).decode('utf-8')
+        elif sys.platform == 'win32':
+            ret = ''.join(os.popen("cscript %s" % path).readlines()[3:]).strip()
+        os.remove(path)
+        return ret
 
-	def urlopen(self, url):
-		for i in range(10):
-			try:
-				print url
-				r = self.opener.open(url,timeout=5)
-				return r
-			except:
-				import traceback
-				print '??', i , url
-				print traceback.format_exc()
-				import time
-				time.sleep(.5)
-				pass
+    def path(self):
+        return self.__class__.__name__
 
-	def untag(self, s):
-		return re.sub("<.*?>", "", s);
+    def urlopen(self, url, opts=None):
+        for i in range(10):
+            try:
+                headers = self.header.copy()
+                if opts:
+                    headers.update(opts)
+                r = requests.get(url, headers=headers, timeout=5)
+                r.encoding = self.encoding
+                return r.text
+            except:
+                import traceback
+                print('??', i, url)
+                print(traceback.format_exc())
+                import time
+                time.sleep(.5)
+                pass
 
-	def chdir(self, p):
-		if not type(p) == unicode:
-			p = p.decode('utf-8')
-		try:
-			os.mkdir(p)
-			os.chdir(p)
-			return True
-		except:
-			os.chdir(p)
-			return False
-		
-	def getPic(self, url, opts={}):
-		if os.path.exists('/usr/local/bin/wget'):
-			for x in range(3):
-					if os.system('/usr/local/bin/wget %s -c "%s"'%(' '.join(map(lambda i:"%s %s"%i, opts.items())), url)) == 0:
-						break
-		else:
-			req = urllib2.Request(url)
-			if '--referer' in opts:
-				req.add_header('Referer', opts['--referer'])
-			open(url.rsplit('/', 1)[-1].split('?', 1)[0], 'wb').write(self.urlopen(req).read())
+    def untag(self, s):
+        return re.sub("<.*?>", "", s)
 
-	
-	def comicPath(self, i):
-		return
+    def chdir(self, p):
+        if isPy2 and not type(p) == unicode:
+            p = p.decode('utf-8')
+        try:
+            os.mkdir(p)
+            os.chdir(p)
+            return True
+        except:
+            os.chdir(p)
+            return False
 
-	def toUrl(self, url):
-		return
-	def getCid(self, url):
-		return
-	def getTitle(self, page):
-		return
-	def getVolumnsUrl(self, url, page, skip=0):
-		return
-	def getVolumn(self, url, force=True):
-		return
+    def getPic(self, url, opts={}):
+        if os.path.exists('/usr/local/bin/wget'):
+            for x in range(3):
+                if os.system(
+                    '/usr/local/bin/wget %s -c "%s"' % (
+                        ' '.join(map(lambda i: "%s %s" % i, opts.items())), url)) == 0:
+                    break
+        else:
+            headers = None
+            if '--referer' in opts:
+                headers = {'Referer': opts['--referer']}
+            open(url.rsplit('/', 1)[-1].split('?', 1)[0], 'wb').write(self.urlopen(url, headers))
 
-	def getAll(self, url,skip=0,force=True):
-		self.chdir(self.path())
-		url = self.toUrl(url)
-		page = self.urlopen(url).read()
-		volumns = self.getVolumnsUrl(url, page, skip)
-		cid = self.getCid(url)
-		title = self.getTitle(page)
-		self.chdir(title)
-		ch = []
-		for url in volumns:
-			c = self.getVolumn(url, force)
-			if c:
-				ch.append(c)
-		os.chdir('../..')
-		return [(title, cid, c) for c in ch]
-	
-	def notify(self, updateList):
-		import pkgutil
-		if not pkgutil.find_loader('pync'):
-			return
-		try:
-			import pync
-			t = '  '.join(['%s-%s'%(n[0].split()[0],n[2]) for n in updateList])
-			if type(t) == unicode:
-				t = t.encode('utf8')
-			pync.Notifier.notify(t, title=self.__class__.__name__, sound = 'ping', execute='open %s -a /Applications/Simple\ Comic.app/'%' '.join(map(self.comicPath, updateList)))
-		except:
-			import traceback
-			traceback.print_exc()
+    def comicPath(self, i):
+        return
 
-	def getUpdate(self):
-		updateList = filter(None, sum([self.getAll(i, -2, False) for i in self.trackList()], []))
-		if len(updateList) > 0:
-			self.notify(updateList)
+    def toUrl(self, url):
+        return
+
+    def getCid(self, url):
+        return
+
+    def getTitle(self, page):
+        return
+
+    def getVolumnsUrl(self, url, page, skip=0):
+        return
+
+    def getVolumn(self, url, force=True):
+        return
+
+    def getAll(self, url, skip=0, force=True):
+        self.chdir(self.path())
+        url = self.toUrl(url)
+        page = self.urlopen(url)
+        volumns = self.getVolumnsUrl(url, page, skip)
+        cid = self.getCid(url)
+        title = self.getTitle(page)
+        self.chdir(title)
+
+        ch = []
+        for urlparam in volumns:
+            c = self.getVolumn(urlparam, force)
+            if c:
+                ch.append(c)
+
+        os.chdir('../..')
+
+        for c in ch:
+            yield (title, cid, c)
+
+    def notify(self, updateList):
+        import pkgutil
+        if not pkgutil.find_loader('pync'):
+            return
+        try:
+            import pync
+            t = '  '.join(['%s-%s' % (n[0].split()[0], n[2]) for n in updateList])
+            cmd = 'open %s -a /Applications/Simple\ Comic.app/' % ' '.join(
+                map(self.comicPath, updateList)
+            )
+            if isPy2:
+                t = t.encode('utf8')
+                cmd = cmd.encode('utf8')
+            pync.Notifier.notify(t, title=self.__class__.__name__, sound='ping', execute=cmd)
+        except:
+            import traceback
+            traceback.print_exc()
+
+    def getUpdate(self):
+        updateList = [comic for i in self.trackList() for comic in self.getAll(i, -2, False)]
+        if updateList:
+            self.notify(updateList)
+
 
 def main():
-	import comic8, dm5
-	comic8.comic8().getUpdate()
-	dm5.dm5().getUpdate()
+    import comic8
+    import dm5
+    comic8.comic8().getUpdate()
+    dm5.dm5().getUpdate()
+
 
 if __name__ == '__main__':
-	main()
+    main()
--- src/pycomics/main.py
+++ src/pycomics/main.py
@@ -1,35 +1,36 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
 def main():
-	from argparser import args, parser
-	from dm5 import dm5
-	from comic8 import comic8
-	dm5 = dm5()
-	comic8 = comic8()
-	
-	if args.u:
-		dm5.getUpdate()
-		comic8.getUpdate()
-	elif args.o:
-		if dm5.path() == args.o:
-			dm5.openTrackList()
-		elif comic8.path() == args.o:
-			comic8.openTrackList()
-	elif args.d:
-		i = 0
-		while i < len(args.d):
-			c = dm5 if 'dm5' in args.d[i] else comic8
-			if i+1 < len(args.d)  and args.d[i+1][-1:].isdigit():
-				c.getAll(args.d[i], int(args.d[i+1]))
-				i += 1
-			else:
-				c.getAll(args.d[i])
-			i += 1
-	else:
-		parser.print_help()
-		parser.exit()
+    from argparser import args, parser
+    from dm5 import dm5
+    from comic8 import comic8
+    dm5 = dm5()
+    comic8 = comic8()
+
+    if args.u:
+        dm5.getUpdate()
+        comic8.getUpdate()
+    elif args.o:
+        if dm5.path() == args.o:
+            dm5.openTrackList()
+        elif comic8.path() == args.o:
+            comic8.openTrackList()
+    elif args.d:
+        i = 0
+        while i < len(args.d):
+            c = dm5 if 'dm5' in args.d[i] else comic8
+            if i + 1 < len(args.d) and args.d[i + 1][-1:].isdigit():
+                c.getAll(args.d[i], int(args.d[i + 1]))
+                i += 1
+            else:
+                c.getAll(args.d[i])
+            i += 1
+    else:
+        parser.print_help()
+        parser.exit()
 
 
 if __name__ == '__main__':
-	main()
+    main()
