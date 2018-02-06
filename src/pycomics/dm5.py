53d8e7655f5b1089f9608491a9e8390cdf64f809
Jim Chang <cwebb.tw@gmail.com>
2018-02-05 17:25:56 -0800

    fix script

--- src/pycomics/dm5.py
+++ src/pycomics/dm5.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import urllib2
+import urllib
 import re
 from ComicSite import *
 
@@ -17,11 +17,11 @@ class dm5(ComicSite):
 		return url.strip('/').split('/')[-1]
 
 	def getTitle(self, page):
-		return re.search('<h1 class="new_h2">(.*?)</h1>',page).group(1)
+		return re.search('var DM5_COMIC_MNAME="(.*?)"', page).group(1)
 
 	def getVolumnsUrl(self, url, page, skip=0):
-		volumns = re.findall('<a class="tg" href="/(.*?)/" title=".*?">([^<]*?)</a>',page)[::-1]
-		return map(lambda (c, n): (c, "%s-%s"%(n.replace('&nbsp;', ' ').split('漫画 ')[-1].strip(), c)), volumns[skip:])
+		volumns = re.findall('<li><a href="/(m[0-9]*)/"[^>]*?>([^<]*?)<', page)[::-1]
+		return map(lambda (c, n): (c, "%s-%s"%(n.replace('&nbsp;', ' ').split()[0].strip(), c)), volumns[skip:])
 
 	def comicPath(self, i):
 		return '"%s/%s/%s/%s"'%(self.root, self.path(), i[0], i[2])
@@ -30,27 +30,25 @@ class dm5(ComicSite):
 		if not self.chdir(name) and not force:
 			os.chdir('..')
 			return
-		i = 1
-		urls = []
-		while True:
-			url = 'http://www.dm5.com/%s-p1/chapterfun.ashx?cid=%s&page=%d&key=&language=1&gtk=6'%(cid,cid[1:],i)
+		page = self.urlopen('http://www.dm5.com/%s/'%cid).read()
+		CID = re.search('DM5_CID=([0-9]*);', page).group(1)
+		MID = re.search('DM5_MID=([0-9]*);', page).group(1)
+		DT = re.search('DM5_VIEWSIGN_DT="([^"]*)"', page).group(1)
+		SIGN = re.search('DM5_VIEWSIGN="([^"]*)"', page).group(1)
+		pcount = int(re.search('DM5_IMAGE_COUNT=([0-9]*);', page).group(1))
+
+		p = 1
+		while p<=pcount:
+			params = urllib.urlencode({'cid':CID, 'page':p, 'key':'', 'language':'1', 'gtk':'6', '_cid':CID, '_mid':MID, '_dt':DT, '_sign':SIGN})
+			url = 'http://www.dm5.com/%s/chapterfun.ashx?%s'%(cid, params)
 			req = urllib2.Request(url)
 			req.add_header('Referer', 'http://www.dm5.com/%s/'%cid)
 			page = self.urlopen(req).read()
 			u = self.execJs("print (%s)"%page).split(',')
-			if len(u) > 1:
-				i += len(u)
-				urls += u
-				for url in u:
-					self.getPic(url, {'--referer': 'http://www.dm5.com/%s/'%cid,
-					'-O': url.rsplit('/',1)[-1].split('?')[0] })
-			else:
-				if u[0] not in urls:
-					urls.append(u[0])
-					url = u[0]
-					self.getPic(url, {'--referer': 'http://www.dm5.com/%s/'%cid,
-					'-O': url.rsplit('/',1)[-1].split('?')[0] })
-				break
+			for url in u:
+				self.getPic(url, {'--referer': 'http://www.dm5.com/%s/'%cid,
+				'-O': url.rsplit('/',1)[-1].split('?')[0] })
+			p += len(u)
 		os.chdir('..')
 		return name
 
